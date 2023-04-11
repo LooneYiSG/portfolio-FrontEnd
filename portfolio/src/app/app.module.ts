071c44586009b697d23a1f7422668ccc6b939e92
LooneYiSG <samu_vm@hotmail.com.ar>
2023-04-11 16:43:09 -0300

    Incorporacion del diseño de frontend

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { WorkComponent } from './components/work/work.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProjectsComponent,
+    WorkComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
--- portfolio/src/app/components/header/header.component.ts
+++ portfolio/src/app/components/header/header.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+    openLogin(): void{
+      console.log('open login')
+    }
+
 }
